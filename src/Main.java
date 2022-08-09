ad525f42699e9a6093854b94e115b2e594a76473
APotekhin1 <alex01210@gmail.com>
2022-08-09 11:36:17 +0300

    int to long in sales

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        int[] sales = { 10, 45, 599, 432, 23 };
+        long[] sales = { 10, 45, 599, 432, 23 };
         SalesManager manager = new SalesManager(sales);
         System.out.println("Максимум: " + manager.max());
     }
