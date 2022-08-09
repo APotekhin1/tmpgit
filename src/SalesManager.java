51f19a62e9b85cbbcad880785deb15a47ce92680
APotekhin1 <alex01210@gmail.com>
2022-08-09 12:20:25 +0300

    Conflict resolved

--- src/SalesManager.java
+++ src/SalesManager.java
@@ -14,18 +14,18 @@ public class SalesManager {
         }
         return max;
     }
-    public int min() {
-        int min = Integer.MIN_VALUE;
-        for (int sale : sales) {
+    public long min() {
+        long min = long.MAX_VALUE;
+        for (long sale : sales) {
             if (sale < min) {
                 min = sale;
             }
         }
         return min;
     }
-    public int avgTrimmed() {
-        int sum = 0;
-        for (int sale : sales) {
+    public long avgTrimmed() {
+        long sum = 0;
+        for (long sale : sales) {
             sum += sale;
         }
         return (sum - min() - max()) / (sales.length - 2);
